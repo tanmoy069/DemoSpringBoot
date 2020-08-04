7bfec48967195bdbee09bf26c23e0fdca34a73ee
tanmo <tanmo@DESKTOP-NETG37O>
2020-08-04 18:16:11 +0600

    Successfully data inserted and retrieved from db

--- src/test/java/com/rokomari/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/rokomari/demo/DemoProjectApplicationTests.java
@@ -1,13 +1,54 @@
 package com.rokomari.demo;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rokomari.demo.dao.UserRepo;
+import com.rokomari.demo.model.User;
 
 @SpringBootTest
 class DemoProjectApplicationTests {
 
+	@Autowired
+	private UserRepo userRepo;
+	
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void findUser() {
+		List<User> userList = userRepo.findAll();
+		System.out.println(userList.size());
+		for (User user : userList) {
+			System.out.println(user.toString());
+		}
+	}
+	
+	@Test
+	void addUser() {
+		try {
+			User user = new User();
+			user.setUserName("tanmoy069");
+			user.setFirstName("Tanmoy");
+			user.setLastName("Tushar");
+			user.setAddress("Mymensingh, Dhaka");
+			user.setAge(25);
+			user.setEmail("pdt.tanmoy@gmail.com");
+			user.setMaritalStatus("Single");
+			user.setProfession("Student");
+			user.setPhone(1672125148);
+			userRepo.save(user);
+			System.out.println("Successfully user added");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	
 
 }
