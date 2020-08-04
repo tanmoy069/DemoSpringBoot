248d2e1472772a361d44d5d5cc20638ea664298c
tanmo <tanmo@DESKTOP-NETG37O>
2020-08-04 23:18:36 +0600

    UserList page has been added with successfully showing user list

--- src/main/java/com/rokomari/demo/controller/HomeController.java
+++ src/main/java/com/rokomari/demo/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.rokomari.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rokomari.demo.dao.UserRepo;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private UserRepo userRepo;
+	
     @RequestMapping("/")
     public String home(){
         return "index";
+    }
+    
+    @GetMapping("/userlist")
+    public String getUserList(Model model){
+    	model.addAttribute("userObj", userRepo.findAll());
+        return "UserList";
     }
 }
