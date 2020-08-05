ac80c5cac5641f9a392d56f6994fb665fe34980a
tanmo <tanmo@DESKTOP-NETG37O>
2020-08-05 13:09:05 +0600

    UserUpdate template has been created and successfully updated user

--- src/main/java/com/rokomari/demo/controller/HomeController.java
+++ src/main/java/com/rokomari/demo/controller/HomeController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.rokomari.demo.dao.UserRepo;
+import com.rokomari.demo.model.User;
 
 @Controller
 public class HomeController {
@@ -35,6 +38,33 @@ public class HomeController {
     public String getUser(Model model, @RequestParam(value="id") int userId){
     	model.addAttribute("userDetails", userRepo.findUserByUserId(userId));
         return "UserView";
+    }
+    
+    @GetMapping("/userupdate")
+    public String userUpdate(Model model, @RequestParam(value="id") int userId){
+    	model.addAttribute("userDetails", userRepo.findUserByUserId(userId));
+        return "UserUpdate";
+    }
+    
+    @PostMapping("/userupdate")
+    public String updateUser(@ModelAttribute(name="updateUserForm") User user, Model model, @RequestParam(value="id") int userId){
+    	model.addAttribute("userDetails", userRepo.findUserByUserId(userId));
+    	try {
+        	User currUser = userRepo.findUserByUserId(userId);
+        	currUser.setFirstName(user.getFirstName());
+        	currUser.setLastName(user.getLastName());
+        	currUser.setAddress(user.getAddress());
+        	currUser.setAge(user.getAge());
+        	currUser.setEmail(user.getEmail());
+        	currUser.setMaritalStatus(user.getMaritalStatus());
+        	currUser.setPhone(user.getPhone());
+        	currUser.setProfession(user.getProfession());
+        	userRepo.save(currUser);
+        	model.addAttribute("validCredentials", true);
+		} catch (Exception e) {
+			model.addAttribute("invalidCredentials", true);
+		}
+        return "UserUpdate";
     }
     
 }
