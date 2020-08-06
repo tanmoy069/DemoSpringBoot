6cf7e11585d2a5033e950756b6221eb59b4606cd
tanmo <tanmo@DESKTOP-NETG37O>
2020-08-06 15:33:03 +0600

    MainService class has been created to new .service package

--- src/main/java/com/rokomari/demo/controller/UserController.java
+++ src/main/java/com/rokomari/demo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.rokomari.demo.controller;
 
-import com.rokomari.demo.domain.User;
-import com.rokomari.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,46 +9,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.rokomari.demo.domain.User;
+import com.rokomari.demo.service.MainService;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
 	@Autowired
-	private UserRepository userRepo;
+	private MainService mainService;
 
 	@GetMapping("/list")
 	public String getUserList(Model model) {
-		model.addAttribute("userObj", userRepo.findAll());
+		model.addAttribute("userObj", mainService.findAll());
 		return "UserList";
 	}
 
 	@GetMapping("/view")
 	public String getUser(Model model, @RequestParam(value = "id") int userId) {
-		model.addAttribute("userDetails", userRepo.findUserByUserId(userId));
+		model.addAttribute("userDetails", mainService.findUserByUserId(userId));
 		return "UserView";
 	}
 
 	@GetMapping("/update")
 	public String userUpdate(Model model, @RequestParam(value = "id") int userId) {
-		model.addAttribute("userDetails", userRepo.findUserByUserId(userId));
+		model.addAttribute("userDetails", mainService.findUserByUserId(userId));
 		return "UserUpdate";
 	}
 
 	@PostMapping("/update")
 	public String updateUser(@ModelAttribute(name = "updateUserForm") User user, Model model,
 			@RequestParam(value = "id") int userId) {
-		model.addAttribute("userDetails", userRepo.findUserByUserId(userId));
+		model.addAttribute("userDetails", mainService.findUserByUserId(userId));
 		try {
-			User currUser = userRepo.findUserByUserId(userId);
-			currUser.setFirstName(user.getFirstName());
-			currUser.setLastName(user.getLastName());
-			currUser.setAddress(user.getAddress());
-			currUser.setAge(user.getAge());
-			currUser.setEmail(user.getEmail());
-			currUser.setMaritalStatus(user.getMaritalStatus());
-			currUser.setPhone(user.getPhone());
-			currUser.setProfession(user.getProfession());
-			userRepo.save(currUser);
+			user.setUserId(userId);
+			mainService.updateUser(user);
 			model.addAttribute("validCredentials", true);
 		} catch (Exception e) {
 			model.addAttribute("invalidCredentials", true);
