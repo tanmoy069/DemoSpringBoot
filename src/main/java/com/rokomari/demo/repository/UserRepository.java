45f884385eebc39f62a79927e905b76b54cf44ad
tanmo <tanmo@DESKTOP-NETG37O>
2020-08-06 11:55:39 +0600

    com.rokomari.demo.dao change to com.rokomari.demo.repository and userRepo change to UserRepository

--- src/main/java/com/rokomari/demo/repository/UserRepository.java
+++ src/main/java/com/rokomari/demo/repository/UserRepository.java
@@ -1,14 +1,15 @@
-package com.rokomari.demo.dao;
+package com.rokomari.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rokomari.demo.model.User;
+import com.rokomari.demo.domain.User;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>{
 	
 	User findUserByUserId(int id);
+	
 	User findByUsername(String username);
 
 }
