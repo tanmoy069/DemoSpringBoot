4efffc663cf97a2efd1e93e339540bd002cfa766
Tridiv <roytridiv1996@gmail.com>
2020-08-06 13:55:45 +0600

    User Controller Created And errors resolved

--- src/main/java/com/rokomari/demo/controller/UserController.java
+++ src/main/java/com/rokomari/demo/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.rokomari.demo.controller;
 
-import com.rokomari.demo.dao.UserRepo;
+import com.rokomari.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
     @Autowired
-    private UserRepo userRepo;
+    private UserRepository userRepo;
 
 }
